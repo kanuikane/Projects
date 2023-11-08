ee5b195e635d25feb89747073106de3d08e49b07
kanuikane <118587949+kanuikane@users.noreply.github.com>
2023-11-08 05:02:58 +0000

    Add files via upload

---
+++ LoginScreen.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace C969
+{
+    partial class LoginScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblLogingUsername = new System.Windows.Forms.Label();
+            this.lblLoginPassword = new System.Windows.Forms.Label();
+            this.txtLoginUsername = new System.Windows.Forms.TextBox();
+            this.txtLoginPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnLoginCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblLogingUsername
+            // 
+            this.lblLogingUsername.AutoSize = true;
+            this.lblLogingUsername.Location = new System.Drawing.Point(73, 74);
+            this.lblLogingUsername.Name = "lblLogingUsername";
+            this.lblLogingUsername.Size = new System.Drawing.Size(55, 13);
+            this.lblLogingUsername.TabIndex = 0;
+            this.lblLogingUsername.Text = "Username";
+            // 
+            // lblLoginPassword
+            // 
+            this.lblLoginPassword.AutoSize = true;
+            this.lblLoginPassword.Location = new System.Drawing.Point(75, 123);
+            this.lblLoginPassword.Name = "lblLoginPassword";
+            this.lblLoginPassword.Size = new System.Drawing.Size(53, 13);
+            this.lblLoginPassword.TabIndex = 1;
+            this.lblLoginPassword.Text = "Password";
+            // 
+            // txtLoginUsername
+            // 
+            this.txtLoginUsername.Location = new System.Drawing.Point(137, 67);
+            this.txtLoginUsername.Name = "txtLoginUsername";
+            this.txtLoginUsername.Size = new System.Drawing.Size(100, 20);
+            this.txtLoginUsername.TabIndex = 2;
+            // 
+            // txtLoginPassword
+            // 
+            this.txtLoginPassword.Location = new System.Drawing.Point(135, 116);
+            this.txtLoginPassword.Name = "txtLoginPassword";
+            this.txtLoginPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtLoginPassword.TabIndex = 3;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(78, 156);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnLoginCancel
+            // 
+            this.btnLoginCancel.Location = new System.Drawing.Point(205, 156);
+            this.btnLoginCancel.Name = "btnLoginCancel";
+            this.btnLoginCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnLoginCancel.TabIndex = 5;
+            this.btnLoginCancel.Text = "Cancel";
+            this.btnLoginCancel.UseVisualStyleBackColor = true;
+            this.btnLoginCancel.Click += new System.EventHandler(this.btnLoginCancel_Click);
+            // 
+            // LoginScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(319, 211);
+            this.Controls.Add(this.btnLoginCancel);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtLoginPassword);
+            this.Controls.Add(this.txtLoginUsername);
+            this.Controls.Add(this.lblLoginPassword);
+            this.Controls.Add(this.lblLogingUsername);
+            this.Name = "LoginScreen";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblLogingUsername;
+        private System.Windows.Forms.Label lblLoginPassword;
+        private System.Windows.Forms.TextBox txtLoginUsername;
+        private System.Windows.Forms.TextBox txtLoginPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnLoginCancel;
+    }
+}
+
---
+++ LoginScreen.cs
@@ -0,0 +1,167 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C969
+{
+    public partial class LoginScreen : Form
+    {
+        public LoginScreen()
+        {
+            InitializeComponent();
+        }
+
+        private bool isUpcoming()
+        {
+            string connString = "server=127.0.0.1;uid=sqlUser;pwd=Passw0rd!;database=client_schedule";
+            string username = txtLoginUsername.Text;
+            bool result = false;
+            try
+            {
+                var conn = new MySqlConnection();
+                conn.ConnectionString = connString;
+                conn.Open();
+
+                string upcomingAppointmentQuery =
+                    $"SELECT * FROM appointment WHERE start BETWEEN NOW() AND NOW() + INTERVAL 15 MINUTE AND userId=(SELECT userId FROM user WHERE userName='{username}')";
+                var upcomingAppointmentCMD = new MySqlCommand(upcomingAppointmentQuery, conn);
+                int upcomingAppointmentIndex = Convert.ToInt32(upcomingAppointmentCMD.ExecuteScalar());
+
+                if (upcomingAppointmentIndex == 0)
+                {
+                    result = false;
+                }
+                else
+                {
+                    result = true;
+                }
+
+            }
+            catch (MySqlException)
+            {
+                MessageBox.Show("Nope Error!");
+            }
+            return result;
+        }
+
+        private void btnLoginCancel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string connString = "server=127.0.0.1;uid=sqlUser;pwd=Passw0rd!;database=client_schedule";
+
+            try
+            {
+                string path = @"log.txt";
+
+                var conn = new MySqlConnection();
+                conn.ConnectionString = connString;
+                conn.Open();
+                string username = txtLoginUsername.Text;
+                string password = txtLoginPassword.Text;
+                string query = $"SELECT * FROM user WHERE userName='{username}' AND password='{password}'";
+                var cmd = new MySqlCommand(query, conn);
+                var reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    Main mainForm = new Main();
+                    if (isUpcoming() != true)
+                    {
+                        MessageBox.Show("Successful Login!");
+                        mainForm.Show();
+                        this.Hide();
+
+                        //Successful login attempts filed
+                        try
+                        {
+                            if (File.Exists(path) == false)
+                            {
+                                File.Create(path).Dispose();
+
+                                using (TextWriter tw = new StreamWriter(path))
+                                {
+                                    tw.WriteLine($"SUCCESSFUL LOGIN ATTEMPT FOR USER {username} AT {DateTime.Now} {Environment.NewLine}");
+                                }
+                            }
+                            else if (File.Exists(path) == true)
+                            {
+                                File.AppendAllText(path, $"SUCCESSFUL LOGIN ATTEMPT FOR USER {username} AT {DateTime.Now} {Environment.NewLine}");
+                            }
+
+                        }
+                        catch (IOException error)
+                        {
+                            MessageBox.Show(error.ToString());
+                        }
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Your upcoming appointment is in 15 minutes.");
+                        mainForm.Show();
+                        this.Hide();
+                    }
+
+                }
+                else
+                {
+                    //Check for English
+                    if (CultureInfo.CurrentCulture.TwoLetterISOLanguageName == "en")
+                    {
+                        MessageBox.Show("Username or Password is incorrect. Please try again.");
+                    }
+                    //Check for Spanish
+                    else if (CultureInfo.CurrentCulture.TwoLetterISOLanguageName == "es")
+                    {
+                        MessageBox.Show("El nombre de usuario o la contraseña son incorrectos. Por favor, inténtelo de nuevo.");
+                    }
+
+                    try
+                    {
+                        if (File.Exists(path) == false)
+                        {
+                            File.Create(path).Dispose();
+
+                            using (TextWriter tw = new StreamWriter(path))
+                            {
+                                tw.WriteLine($"UNSUCCESSFUL LOGIN ATTEMPT FOR USER {username} AT {DateTime.Now} {Environment.NewLine}");
+                            }
+                        }
+                        else if (File.Exists(path) == true)
+                        {
+                            File.AppendAllText(path, $"UNSUCCESSFUL LOGIN ATTEMPT FOR USER {username} AT {DateTime.Now} {Environment.NewLine}");
+                        }
+
+                    }
+                    catch (IOException error)
+                    {
+                        MessageBox.Show(error.ToString());
+                    }
+                }
+            }
+            catch (MySqlException)
+            {
+                MessageBox.Show("Unable to connect!");
+            }
+            finally
+            {
+
+            }
+
+
+
+        }
+    }
+}
